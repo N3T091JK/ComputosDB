e087e16d77172f45cc58cc23fafdfd4bfadf48ef
Ernesto <pa0878022021@unab.edu.sv>
2023-05-17 17:45:32 -0600

    Realizace cambios en la BL

---
+++ ComputosDB.Entities/Computadoras.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputosDB.Entities
+{
+    public class Computadoras
+    {
+        [Key]
+        public int idComputadora { get; set; }
+        [MaxLength(80)]
+        [Required]
+        public string Nombre { get; set; }
+        public virtual ICollection<Detallecomputadora> Detallecomputadoras { get; set; }
+
+    }
+}
---
+++ ComputosDB.Entities/Detallecomputadora.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputosDB.Entities
+{
+    public class Detallecomputadora
+    {
+        [Key]
+        public int IdDetallecomputadora { get; set; }
+        [MaxLength(80)]
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public string Ram { get; set; }
+        [Required]
+        public string ModeloDePlaca { get; set; }
+        [Required]
+        public string Teclado { get; set; }
+        [Required]
+        public string Raton { get; set; }
+        [Required]
+        public string Procesador { get; set; }
+        [Required]
+        public int NumerodeRAM { get; set; }
+        [Required]
+        public string Nucleos { get; set; }
+        [Required]
+        public decimal PulgadasDepantalla { get; set; }
+        [Required]
+        public int idComputadora { get; set; }
+        public virtual Computadoras Computadoras { get; set; }
+    }
+}
